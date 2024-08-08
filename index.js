b043f023d94f47c0ba7b0ccc2717cc774bfed902
Anurag Parashar Sarmah <anuragp787@gmail.com>
2024-08-08 21:17:08 +0530

    fixes

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 
 // CORS middleware
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173'); // Replace with the origin of your frontend
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
   res.setHeader('Access-Control-Allow-Credentials', true);
