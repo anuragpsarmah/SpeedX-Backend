aa2c20e601836de966bf4d71df88742df7f03c9a
Anurag Parashar Sarmah <anuragp787@gmail.com>
2024-08-08 21:16:39 +0530

    Fixes

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 
 // CORS middleware
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*'); // Adjust as needed
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, content-type');
   res.setHeader('Access-Control-Allow-Credentials', true);
